1f2f227c4150fe8fd5415f588b228ee0f89d9f33
Stefan Insam <ramsesoriginal@gmail.com>
2014-08-16 13:41:44 +0200

    Added ability to take screenshots

---
+++ Assets/Entities/MainCamera/ScreenShot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MainCamera {
+	public class ScreenShot : MonoBehaviour {
+
+		private string path;
+
+		void Start () {
+			if (!System.IO.Directory.Exists ("Screenshot")) {
+				System.IO.Directory.CreateDirectory("Screenshot");
+			}
+			path = "Screenshot" + System.IO.Path.DirectorySeparatorChar;
+		}
+
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetKeyDown (KeyCode.J)) {
+				Application.CaptureScreenshot(path + "Screenshot"+System.DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")+".png");
+			}
+			if (Input.GetKeyDown (KeyCode.K)) {
+				Application.CaptureScreenshot(path + "Screenshot"+System.DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")+".png",3);
+			}
+		}
+	}
+}
--- Assets/Entities/Player/Locomotion.cs
+++ Assets/Entities/Player/Locomotion.cs
@@ -1,31 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Locomotion : MonoBehaviour {
+namespace Player {
+	public class Locomotion : MonoBehaviour {
 
-	public Transform unrotated;
-	public float Force = 4;
-	public float Acceleration = 4;
+		public Transform unrotated;
+		public float Force = 4;
+		public float Acceleration = 4;
 
-	public bool player2;
+		public bool player2;
 
+		// Update is called once per frame
+		void Update () {
+			var playerText = "Player 1";
+			if (player2)
+				playerText = "Player 2";
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		var playerText = "Player 1";
-		if (player2)
-			playerText = "Player 2";
-
-		if (Input.GetAxis (playerText + " Horizontal")>0.1 || Input.GetAxis (playerText + " Horizontal")< -0.1 ) {
-			rigidbody.AddForce(Time.deltaTime * unrotated.right * Input.GetAxis (playerText + " Horizontal") * Force,ForceMode.VelocityChange);
-		}
-		if (Input.GetAxis (playerText + " Vertical")>0.1 || Input.GetAxis (playerText + " Vertical")< -0.1 ) {
-			rigidbody.AddForce(Time.deltaTime * unrotated.forward * Input.GetAxis (playerText + " Vertical") * Acceleration,ForceMode.VelocityChange);
+			if (Input.GetAxis (playerText + " Horizontal")>0.1 || Input.GetAxis (playerText + " Horizontal")< -0.1 ) {
+				rigidbody.AddForce(Time.deltaTime * unrotated.right * Input.GetAxis (playerText + " Horizontal") * Force,ForceMode.VelocityChange);
+			}
+			if (Input.GetAxis (playerText + " Vertical")>0.1 || Input.GetAxis (playerText + " Vertical")< -0.1 ) {
+				rigidbody.AddForce(Time.deltaTime * unrotated.forward * Input.GetAxis (playerText + " Vertical") * Acceleration,ForceMode.VelocityChange);
+			}
 		}
 	}
 }
