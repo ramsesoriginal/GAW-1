3dd560d741e420e14bb7b4eaaac496313c9bb9c7
Stefan Insam <ramsesoriginal@gmail.com>
2014-08-21 18:37:23 +0200

    Now we have functioning player Counter and ranking

---
+++ Assets/Entities/GUI/CountPlayers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace CustomGUI {
+	public class CountPlayers : MonoBehaviour {
+
+		public int playerCount;
+		public int currentPlayerCount;
+		public Transform[] ranking;
+
+		private List<Transform> players;
+
+		// Use this for initialization
+		void Start () {
+			currentPlayerCount = countPlayers ();
+			playerCount = currentPlayerCount;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			playerCount = countPlayers ();
+		}
+
+		int countPlayers() {
+			var locomotions = GetComponentsInChildren<Player.Locomotion> ();
+			players = new List<Transform>();
+			foreach(var l in locomotions) {
+				players.Add(l.transform);
+			}
+			ranking = players.OrderBy (q => -q.position.magnitude).ToArray ();
+			return locomotions.Count();
+		}
+	}
+}
